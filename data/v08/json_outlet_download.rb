426c260825c10db82e418de4068329e2f530f5c5
Mark Koyanagi <mkoyanagi@rti.org>
2018-08-07 14:34:46 -0400

    AK0002 records only

--- data/v08/json_outlet_download.rb
+++ data/v08/json_outlet_download.rb
@@ -3,7 +3,7 @@ require 'json'
 
 client = SODA::Client.new({:domain => "data.imls.gov", :app_token => "Q0kGme0P8YOPOAqZNNTPcTuAY"})
 
-results = client.get("3qh2-qqv7", :$limit => 200)
+results = client.get("3qh2-qqv7", :$limit => 20000)
 
 File.open("json/2016_library_outlets.json", "w") do |f|
   f.write(results.to_json)
