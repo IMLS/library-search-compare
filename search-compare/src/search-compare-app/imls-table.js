05f8d160e2bcb5d1a2f0de78c08efd53efc55a89
Mark Koyanagi <mkoyanagi@rti.org>
2018-09-04 15:58:09 -0400

    Build v10 revisions

--- search-compare/src/search-compare-app/imls-table.js
+++ search-compare/src/search-compare-app/imls-table.js
@@ -1,10 +1,10 @@
 define(["../../node_modules/@polymer/polymer/polymer-element.js", "./csv.js"], function (_polymerElement, _csv) {
   "use strict";
 
-  function _templateObject_0c0d5210afae11e8a1e2f7ec9a1d356b() {
+  function _templateObject_9d61b660b07c11e89593092ec8c12b51() {
     var data = babelHelpers.taggedTemplateLiteral(["\n      <style>\n        :host {\n          display: block;\n        }\n      </style>\n      <slot></slot>\n    "]);
 
-    _templateObject_0c0d5210afae11e8a1e2f7ec9a1d356b = function _templateObject_0c0d5210afae11e8a1e2f7ec9a1d356b() {
+    _templateObject_9d61b660b07c11e89593092ec8c12b51 = function _templateObject_9d61b660b07c11e89593092ec8c12b51() {
       return data;
     };
 
@@ -52,9 +52,9 @@ define(["../../node_modules/@polymer/polymer/polymer-element.js", "./csv.js"], f
       value: function render() {
         var _this2 = this;
 
-        this.innerHTML = "\n      <div class=\"actions-box\"> \n          <div class=\"row\"> \n              <div class=\"col-sm-8\">\n              <div id=\"user-comparison-select-wrapper\">\n                  <span id=\"user-comparison-select-text\">See variables related to: </span>\n                  <select id=\"user-comparison-select\">\n                  ".concat(this._comparisonTableConfig.map(function (option) {
+        this.innerHTML = "\n      <div class=\"actions-box\"> \n          <div class=\"row\">\n              <div class=\"col-sm-4\">\n              <span id=\"userCount\"></span>\n              ".concat(this.shareUrl !== '' ? "<button id=\"user-share-btn\" class=\"btn btn-default btn-sm\" type=\"button\">Share These Results</button>" : "", "\n              <div id=\"shareDiv\" class=\"closed\">\n                  <p>This page has been copied to your clipboard. Paste somewhere to share!</p>\n                  <input type=\"text\" value=\"").concat(this.shareUrl, "\" id=\"userShareMe\">\n                  </div><!--end #shareDiv-->\n                  <button id=\"download-user-csv\" class=\"btn btn-default btn-sm\"><i class=\"icon-file-excel\"></i> Download</button>\n              </div>\n              <div class=\"col-sm-8 text-right\">\n              <div id=\"user-comparison-select-wrapper\">\n                  <span id=\"user-comparison-select-text\">See variables related to: </span>\n                  <select id=\"user-comparison-select\">\n                  ").concat(this._comparisonTableConfig.map(function (option) {
           return "\n                    <option value=\"".concat(option.name, "\" ").concat(option.name === _this2.compareOn ? 'selected' : '', ">\n                      ").concat(option.display_name, "\n                    </option>\n                  ");
-        }).join(''), "\n                  </select>\n              </div>\n              </div>\n              <div class=\"col-sm-4 text-right\">\n              <span id=\"userCount\"></span>\n              ").concat(this.shareUrl !== '' ? "<button id=\"user-share-btn\" class=\"btn btn-default btn-sm\" type=\"button\">Share These Results</button>" : "", "\n              <div id=\"shareDiv\" class=\"closed\">\n                  <p>This page has been copied to your clipboard. Paste somewhere to share!</p>\n                  <input type=\"text\" value=\"").concat(this.shareUrl, "\" id=\"userShareMe\">\n                  </div><!--end #shareDiv-->\n                  <button id=\"download-user-csv\" class=\"btn btn-default btn-sm\"><i class=\"icon-file-excel\"></i> Download</button>\n              </div>\n          </div>              \n          </div><!--end .actions-box-->\n          <div id=\"user-comparison-results-wrapper\">\n          <div class=\"table-responsive\">\n              <table class=\"table table-striped\" id=\"data_table\"></table>\n          </div>\n      </div><!-- end user-comparison-results-wrapper -->\n    ");
+        }).join(''), "\n                  </select>\n              </div>\n              </div>\n          </div>              \n          </div><!--end .actions-box-->\n          <div id=\"user-comparison-results-wrapper\">\n          <div class=\"table-responsive\">\n              <table class=\"table table-striped\" id=\"data_table\"></table>\n          </div>\n      </div><!-- end user-comparison-results-wrapper -->\n    ");
         if (this.shareUrl !== '') this.querySelector('#user-share-btn').addEventListener('click', this.showShareUrl.bind(this));
         this.querySelector('#download-user-csv').addEventListener('click', this.downloadCsv.bind(this));
         this.querySelector('#user-comparison-select').addEventListener('change', function (event) {
@@ -271,7 +271,7 @@ define(["../../node_modules/@polymer/polymer/polymer-element.js", "./csv.js"], f
     }], [{
       key: "template",
       get: function get() {
-        return (0, _polymerElement.html)(_templateObject_0c0d5210afae11e8a1e2f7ec9a1d356b());
+        return (0, _polymerElement.html)(_templateObject_9d61b660b07c11e89593092ec8c12b51());
       }
     }, {
       key: "properties",
