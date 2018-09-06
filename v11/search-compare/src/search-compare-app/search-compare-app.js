2e3ded33909fffc05031c3c1f0345334eeb1a8d9
R.J. Steinert <rj@rjsteinert.com>
2018-09-06 16:58:13 -0400

    build v11

---
+++ v11/search-compare/src/search-compare-app/search-compare-app.js
@@ -0,0 +1,48 @@
+define(["../../node_modules/@polymer/polymer/polymer-element.js", "./imls-table.js"], function (_polymerElement, _imlsTable) {
+  "use strict";
+
+  function _templateObject_fcfd3f40b21611e8831057a98fe1bafc() {
+    var data = babelHelpers.taggedTemplateLiteral(["\n      <style>\n        :host {\n          display: block;\n        }\n      </style>\n      <h2>Hello [[prop1]]!</h2>\n    "]);
+
+    _templateObject_fcfd3f40b21611e8831057a98fe1bafc = function _templateObject_fcfd3f40b21611e8831057a98fe1bafc() {
+      return data;
+    };
+
+    return data;
+  }
+
+  /**
+   * @customElement
+   * @polymer
+   */
+  var SearchCompareApp =
+  /*#__PURE__*/
+  function (_PolymerElement) {
+    babelHelpers.inherits(SearchCompareApp, _PolymerElement);
+
+    function SearchCompareApp() {
+      babelHelpers.classCallCheck(this, SearchCompareApp);
+      return babelHelpers.possibleConstructorReturn(this, (SearchCompareApp.__proto__ || Object.getPrototypeOf(SearchCompareApp)).apply(this, arguments));
+    }
+
+    babelHelpers.createClass(SearchCompareApp, null, [{
+      key: "template",
+      get: function get() {
+        return (0, _polymerElement.html)(_templateObject_fcfd3f40b21611e8831057a98fe1bafc());
+      }
+    }, {
+      key: "properties",
+      get: function get() {
+        return {
+          prop1: {
+            type: String,
+            value: 'search-compare-app'
+          }
+        };
+      }
+    }]);
+    return SearchCompareApp;
+  }(_polymerElement.PolymerElement);
+
+  window.customElements.define('search-compare-app', SearchCompareApp);
+});
