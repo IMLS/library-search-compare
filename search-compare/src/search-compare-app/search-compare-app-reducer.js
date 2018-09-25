d64f8aea8449ef62bb2135216a6f63a6126b6d4e
R.J. Steinert <rj@rjsteinert.com>
2018-09-25 13:55:04 -0400

    Remove use of Set that was breaking the ADD_LIBRARY action in IE11

--- search-compare/src/search-compare-app/search-compare-app-reducer.js
+++ search-compare/src/search-compare-app/search-compare-app-reducer.js
@@ -7,7 +7,7 @@ const initialState = {
 export function searchCompareAppReducer(state=initialState, action) {
   switch(action.type) {
     case 'ADD_LIBRARY': 
-      return Object.assign({}, state, { myLibraries: [...new Set([...state.myLibraries, action.id])] })
+      return Object.assign({}, state, { myLibraries: [...state.myLibraries, action.id] })
     case 'REMOVE_LIBRARY':
       return Object.assign({}, state, { myLibraries: state.myLibraries.filter(id => id !== action.id) })
     case 'TOGGLE_SEARCH_MODE':
