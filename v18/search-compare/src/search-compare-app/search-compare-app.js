df74f37f23a017a0b69f582e0e63d5f5e741064b
mkoyanagi <mkoyanagi@rti.org>
2020-06-09 10:48:29 -0400

    interim build for v18

---
+++ v18/search-compare/src/search-compare-app/search-compare-app.js
@@ -0,0 +1,68 @@
+define(["../../node_modules/@polymer/polymer/polymer-element.js", "./imls-table.js", "./search-compare-app-reducer.js"], function (_polymerElement, _imlsTable, _searchCompareAppReducer) {
+  "use strict";
+
+  function _templateObject_0d16ecd0aa6011eaa060fb59a0289b88() {
+    var data = babelHelpers.taggedTemplateLiteral(["\n      <style>\n        :host {\n          display: hidden;\n        }\n      </style>\n    "]);
+
+    _templateObject_0d16ecd0aa6011eaa060fb59a0289b88 = function _templateObject_0d16ecd0aa6011eaa060fb59a0289b88() {
+      return data;
+    };
+
+    return data;
+  }
+
+  /**
+   * @customElement
+   * @polymer
+   */
+  var SearchCompareApp =
+  /*#__PURE__*/
+  function (_PolymerElement) {
+    babelHelpers.inherits(SearchCompareApp, _PolymerElement);
+
+    function SearchCompareApp() {
+      var _this;
+
+      babelHelpers.classCallCheck(this, SearchCompareApp);
+      _this = babelHelpers.possibleConstructorReturn(this, babelHelpers.getPrototypeOf(SearchCompareApp).call(this));
+
+      if (localStorage.getItem('search-compare-app-state')) {
+        _this.store = Redux.createStore(_searchCompareAppReducer.searchCompareAppReducer, JSON.parse(localStorage.getItem('search-compare-app-state')), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+      } else {
+        _this.store = Redux.createStore(_searchCompareAppReducer.searchCompareAppReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+      }
+
+      _this.store.subscribe(function () {
+        return _this.onStateUpdate(_this.store.getState());
+      });
+
+      window.startAppJs();
+      return _this;
+    }
+
+    babelHelpers.createClass(SearchCompareApp, [{
+      key: "onStateUpdate",
+      value: function onStateUpdate(state) {
+        localStorage.setItem('search-compare-app-state', JSON.stringify(state));
+      }
+    }], [{
+      key: "template",
+      get: function get() {
+        return (0, _polymerElement.html)(_templateObject_0d16ecd0aa6011eaa060fb59a0289b88());
+      }
+    }, {
+      key: "properties",
+      get: function get() {
+        return {
+          prop1: {
+            type: String,
+            value: 'search-compare-app'
+          }
+        };
+      }
+    }]);
+    return SearchCompareApp;
+  }(_polymerElement.PolymerElement);
+
+  window.customElements.define('search-compare-app', SearchCompareApp);
+});
